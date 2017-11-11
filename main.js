bee488e1df287d58714198a91bf578833244b7ed
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-11-10 21:41:43 -0600

    Updated JSON

--- main.js
+++ main.js
@@ -2,66 +2,49 @@
 
 console.log("main.js, YO");
 
-let cohort = [];
-cohort.push({
-	title: 'PERSONS NAME',
-    copy: 'I like to cook, eat, code, coffee, and Star Wars. This is filler text so that I can add some stuff here!',
-    github: 'http://www.google.com',
-    linkedin: 'http://www.google.com',
-    portfolio: 'http://www.google.com',
-    img: 'assets/images/avatar-1577909_960_720.png',
-});
-cohort.push({
-	title: 'PERSONS NAME',
-    copy: 'I like to cook, eat, code, coffee, and Star Wars. This is filler text so that I can add some stuff here!',
-    github: 'http://www.google.com',
-    linkedin: 'http://www.google.com',
-    portfolio: 'http://www.google.com',
-    img: 'assets/images/avatar-1577909_960_720.png',
-});
-cohort.push({
-	title: 'PERSONS NAME',
-    copy: 'I like to cook, eat, code, coffee, and Star Wars. This is filler text so that I can add some stuff here!',
-    github: 'http://www.google.com',
-    linkedin: 'http://www.google.com',
-    portfolio: 'http://www.google.com',
-    img: 'assets/images/avatar-1577909_960_720.png',
-});
-cohort.push({
-	title: 'PERSONS NAME',
-    copy: 'I like to cook, eat, code, coffee, and Star Wars. This is filler text so that I can add some stuff here!',
-    github: 'http://www.google.com',
-    linkedin: 'http://www.google.com',
-    portfolio: 'http://www.google.com',
-    img: 'assets/images/avatar-1577909_960_720.png',
-});
-cohort.push({
-	title: 'PERSONS NAME',
-    copy: 'I like to cook, eat, code, coffee, and Star Wars. This is filler text so that I can add some stuff here!',
-    github: 'http://www.google.com',
-    linkedin: 'http://www.google.com',
-    portfolio: 'http://www.google.com',
-    img: 'assets/images/avatar-1577909_960_720.png',
-});
-cohort.push({
-	title: 'PERSONS NAME',
-    copy: 'I like to cook, eat, code, coffee, and Star Wars. This is filler text so that I can add some stuff here!',
-    github: 'http://www.google.com',
-    linkedin: 'http://www.google.com',
-    portfolio: 'http://www.google.com',
-    img: 'assets/images/avatar-1577909_960_720.png',
-});
+$.ajax({
+  url: "data/cohort.json"
+}).done(cohortMembers)
+  .fail(function(error){
+    console.log("error", error);
+  });
 
-cohort.forEach(function(item){
-	document.getElementById("cohort").innerHTML += `<div class="col-sm-4">
-        <img class="card-img-top" src="${item.img}" alt="Card image cap">
-        <div class="card-body">
-          <h4 class="card-title">${item.title}</h4>
-          <p class="card-text">${item.copy}</p>
-          <a href="${item.github}" target= "_blank" class="btn btn-primary">Github</a>
-          <a href="${item.linkedin}" target= "_blank" class="btn btn-primary">LinkedIn</a>
-          <a href="${item.portfolio}" target= "_blank" class="btn btn-primary">Portfolio</a>
+function cohortMembers(list){
+  let data = list.cohort;
+  data.forEach(function(item){
+    document.getElementById("cohort").innerHTML += `<div class="col-sm-3">
+          <img class="card-img-top" src="${item.proimg}" alt="${item.fname} ${item.lname}">
+          <div class="card-body">
+            <h4 class="card-title">${item.fname} ${item.lname}</h4>
+            <p class="card-text">${item.reelthemin}</p>
+            <center><button type="button" class="btn btn-primary bottom" data-toggle="modal" data-target="#cohortMember${item.id}">
+           Meet ${item.fname}!
+          </button></center>
+          </div>
+        </div>
+        <div class="modal fade" id="cohortMember${item.id}" tabindex="-1" role="dialog" aria-labelledby="cohortMember${item.id}Label" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="cohortMember${item.id}Label">${item.fname} ${item.lname}</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+            <center><img src="${item.funimg}" /></center><br>
+            ${item.copy}
+            </div>
+            <div class="modal-footer">
+            <h4>Contact ${item.fname}</h4>
+            <a href="${item.portfolio}" target= "_blank"><img src="assets/images/portfolio.png"></a>
+              <a href="${item.github}"target= "_blank"><img src="assets/images/github.png"></a>
+              <a href="${item.linkedin}" target= "_blank"><img src="assets/images/linkedin.png"></a>
+              <a href="mailto:${item.email}" target= "_blank"><img src="assets/images/email.png"></a>
+            </div>
+          </div>
         </div>
       </div>`;
-	
-});
+    
+  });
+};
